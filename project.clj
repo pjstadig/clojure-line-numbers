f534af97990c6be494723f3339cbc11bffe2436a
Paul Stadig <paul@stadig.name>
2014-10-20 14:47:32 -0400

    Add examples.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot clojure-line-numbers.core
+  :profiles {:fix {:dependencies ^:replace [[name.stadig/clojure "1.7.0-linenumberfix-SNAPSHOT"]]}})
--- src/clojure_line_numbers/core.clj
+++ src/clojure_line_numbers/core.clj
@@ -1,6 +1,65 @@
-(ns clojure-line-numbers.core)
+(ns clojure-line-numbers.core
+  (:require [clojure.stacktrace])
+  (:import (clojure.lang PersistentHashMap)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(set! *warn-on-reflection* true)
+
+(defrecord Thing [field])
+
+(defn instance-field
+  "I throw an exception in an instance field form."
+  []
+  (.field
+   ^Thing (identity nil)))
+
+(defn instance-method
+  "I throw an exception in an instance method form."
+  []
+  (.without
+   ^PersistentHashMap (identity nil)
+   :key))
+
+(defn static-method
+  "I throw an exception in a static method form."
+  []
+  (PersistentHashMap/create
+   ^java.util.Map (identity nil)))
+
+(defn keyword-invoke
+  "I throw an exception in a keyword invoke form."
+  []
+  (:key
+   {}
+   (identity :bar)
+   (identity :baz)))
+
+(defn invoke
+  "I throw an exception in an invoke form."
+  []
+  ((identity nil)
+   (identity nil)))
+
+(defn threading
+  "I throw an exception in a threading form."
+  []
+  (-> :foo
+      (identity)
+      (identity)
+      ((identity nil))
+      (identity)
+      (identity)))
+
+(defmacro print-boom [n & body]
+  `(try
+     ~@body
+     (catch Throwable t#
+       (clojure.stacktrace/print-trace-element (nth (.getStackTrace t#) ~n))
+       (println))))
+
+(defn -main [& args]
+  (print-boom 0 (instance-field))
+  (print-boom 0 (instance-method))
+  (print-boom 1 (static-method))
+  (print-boom 2 (keyword-invoke))
+  (print-boom 0 (invoke))
+  (print-boom 0 (threading)))
