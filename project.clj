962167a33beda4dfe9249823e5b76b11770dedf4
Paul Stadig <paul@stadig.name>
2014-10-20 15:19:10 -0400

    Pin snapshot.

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot clojure-line-numbers.core
-  :profiles {:fix {:dependencies ^:replace [[name.stadig/clojure "1.7.0-linenumberfix-SNAPSHOT"]]}})
+  :profiles {:fix {:dependencies ^:replace [[name.stadig/clojure "1.7.0-linenumberfix-20141020.191148-1"]]}})
